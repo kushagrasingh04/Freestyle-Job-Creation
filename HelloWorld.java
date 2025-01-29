7178052a356ab476e41aeadf877a21e3644a0218
Kushagra Singh <kingsrobert29@gmail.com>
2025-01-29 15:06:49 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// This is a basic Java program
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
